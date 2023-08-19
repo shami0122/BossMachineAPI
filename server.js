01690980b0af9474469d1e95720227ecf1b13dbf
Shami Sharma <shamisharma01@outlook.com>
2023-08-18 21:34:56 -0400

    Mount the api router

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(bodyParser.json());
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
 
+app.use('/api', apiRouter);
+
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
