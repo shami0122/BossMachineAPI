475bb6e26f8e87c520f853f8fb393435aac1696a
Shami Sharma <shamisharma01@outlook.com>
2023-08-19 22:41:27 -0400

    Added a new get request for MinionId

--- server/api.js
+++ server/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const apiRouter = express.Router();
+const { getAllFromDatabase } = require('./db');
 
 const minionsRouter = require('./minions');
 const ideasRouter = require('./ideas');
@@ -9,8 +10,5 @@ apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
 apiRouter.use('/meetings', meetingsRouter);
 
-minionsRouter.get('/', (req, res, next) => {
-    res.send(getAllFromDatabase('minions'));
-})
 
 module.exports = apiRouter;
--- server/minions.js
+++ server/minions.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { getAllFromDatabase } = require('./db');
+const { getAllFromDatabase, getFromDatabaseById } = require('./db');
 const minionsRouter = express.Router();
 
 minionsRouter.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('minions'));
+})
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+    res.send(getFromDatabaseById('minions', req.params.minionId));
 })
 
 module.exports = minionsRouter;
