cfe030b3648ec62ec5ac68bbd73fee983887c9d1
Shami Sharma <shamisharma01@outlook.com>
2023-08-25 22:18:28 -0400

    fixed the post request and added the get/:minionId, put and delete methods

--- server/minions.js
+++ server/minions.js
@@ -1,13 +1,43 @@
 const express = require('express');
-const { getAllFromDatabase, getFromDatabaseById } = require('./db');
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('./db');
 const minionsRouter = express.Router();
 
-minionsRouter.get('/', (req, res, next) => {
+minionsRouter.param('minionId', (req, res, next, id) => {
+    const minion = getFromDatabaseById('minions', id);
+    if(minion){
+        req.minion = minion;
+        next();
+    }
+    else{
+        res.status(404).send();
+    }
+})
+
+minionsRouter.get('/', (req, res) => {
     res.send(getAllFromDatabase('minions'));
 })
 
-minionsRouter.get('/:minionId', (req, res, next) => {
-    res.send(getFromDatabaseById('minions', req.params.minionId));
+minionsRouter.post('/', (req, res) => {
+    const minion = addToDatabase('minions', req.body);
+    res.status(201).send(minion);  
 })
 
+minionsRouter.get('/:minionId', (req, res) => {
+    res.send(req.minion);  
+})
+
+minionsRouter.put('/:minionId', (req, res) => {
+    const minion = updateInstanceInDatabase('minions', req.body);
+    res.status(200).send(minion);
+});
+
+minionsRouter.delete('/:minionId', (req, res) => {
+    console.log(req.params.minionId);
+    const minion = deleteFromDatabasebyId('minions', req.params.minionId);
+    if(minion){
+        res.status(204).send();    
+    }else{
+        res.status(404).send();
+    }
+})
 module.exports = minionsRouter;
